12256b4d2054f86df30b06140ca01e7e59cb6273
Björn Domars <bdomars@walkbase.com>
2026-07-23 20:09:59 +0300

    Remove old env file

--- env.fish
+++
@@ -1,7 +0,0 @@
-set -gx DISCORD_TOKEN replace-with-bot-token
-set -gx GUILD_ID replace-with-guild-id
-
-set -gx OTHER_GAMES_BASE_NAME "🎮│Other Games"
-set -gx OTHER_GAMES_MIN_CHANNELS 3
-set -gx OTHER_GAMES_MAX_CHANNELS 10
-set -gx OTHER_GAMES_IDLE_SECONDS 300
